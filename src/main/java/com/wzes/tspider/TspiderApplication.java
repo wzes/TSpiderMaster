25de4cc4317582db41b7fae0254855d84334bf38
wzes <793890838@qq.com>
2018-01-29 11:35:43 +0800

    first commit

---
+++ src/main/java/com/wzes/tspider/TspiderApplication.java
@@ -0,0 +1,15 @@
+package com.wzes.tspider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuantang
+ */
+@SpringBootApplication
+public class TspiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TspiderApplication.class, args);
+	}
+}
