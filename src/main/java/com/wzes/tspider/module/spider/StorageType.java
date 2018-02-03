1b123c8fa2da3803fbb675bba3a5c1a09f416dbb
wzes <793890838@qq.com>
2018-02-03 20:16:49 +0800

    添加存储类型

--- src/main/java/com/wzes/tspider/module/spider/StorageType.java
+++ src/main/java/com/wzes/tspider/module/spider/StorageType.java
@@ -5,4 +5,12 @@ package com.wzes.tspider.module.spider;
  * @date on 2/3/18
  */
 public enum StorageType {
+    /**
+     * csv 文件
+     */
+    STORAGE_CSV,
+    /**
+     * mysql 数据库
+     */
+    STORAGE_MYSQL
 }
