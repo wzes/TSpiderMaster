48136844ef331c9b9af529cb418622b3ee637adb
wzes <793890838@qq.com>
2018-02-04 21:30:44 +0800

    url 仓库，实现多线程共享

--- src/main/java/com/wzes/tspider/service/spider/UrlWarehouse.java
+++ src/main/java/com/wzes/tspider/service/spider/UrlWarehouse.java
@@ -1,8 +1,78 @@
 package com.wzes.tspider.service.spider;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Create by xuantang
  * @date on 2/4/18
  */
 public class UrlWarehouse {
+    private List<String> urls;
+    private int index;
+    private static UrlWarehouse urlWarehouse;
+
+    private UrlWarehouse() {
+
+    }
+
+    /**
+     * 单例模式
+     *
+     * @return
+     */
+    public static UrlWarehouse getInstance() {
+        if (urlWarehouse == null) {
+            synchronized (UrlWarehouse.class) {
+                if (urlWarehouse == null) {
+                    urlWarehouse = new UrlWarehouse();
+                }
+            }
+        }
+        return urlWarehouse;
+    }
+
+    public List<String> getUrls() {
+        return urls;
+    }
+
+    public void setUrls(List<String> urls) {
+        this.urls = urls;
+    }
+
+    /**
+     * 添加 url
+     * @param url
+     */
+    public void addUrl(String url) {
+        if (urls == null) {
+            this.urls = new ArrayList<>();
+        }
+        urls.add(url);
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+
+    /**
+     * 获取单个 url
+     * @return url
+     */
+    public synchronized String getUrl() {
+        if (urls != null) {
+            if (index < urls.size()) {
+                return urls.get(index++);
+            } else {
+                return "";
+            }
+        } else {
+            throw new NullPointerException();
+        }
+    }
 }
