fb4907a23b0feb830d2a89dc298e29f6148178ed
wzes <793890838@qq.com>
2018-02-05 15:42:18 +0800

    使用全局变量都放在一起，方便管理

---
+++ src/main/java/com/wzes/tspider/service/Constant.java
@@ -0,0 +1,16 @@
+package com.wzes.tspider.service;
+
+/**
+ * @author Create by xuantang
+ * @date on 2/5/18
+ */
+public class Constant {
+    /**
+     *
+     */
+    public static final String RESULT_ITEM_SPLIT_SYMBOL = " ";
+    /**
+     *
+     */
+    public static final String RESULT_LINE_SPLIT_SYMBOL = "\n";
+}
