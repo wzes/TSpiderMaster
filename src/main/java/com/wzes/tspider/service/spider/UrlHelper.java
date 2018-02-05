ec43e0ed162ae4d73e06727cbdfbe9ccdd0175b0
wzes <793890838@qq.com>
2018-02-05 15:43:54 +0800

    获取url绝对路径

--- src/main/java/com/wzes/tspider/service/spider/UrlHelper.java
+++ src/main/java/com/wzes/tspider/service/spider/UrlHelper.java
@@ -1,8 +1,27 @@
 package com.wzes.tspider.service.spider;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * @author Create by xuantang
  * @date on 2/5/18
  */
 public class UrlHelper {
+    /**
+     * 获取绝对链接
+     * @param basePath the base url
+     * @param relativePath the relative url
+     * @return the absolute url
+     */
+    public static String getAbsUrl(String basePath, String relativePath){
+        try {
+            URL baseUrl = new URL(basePath);
+            URL parseUrl = new URL(baseUrl ,relativePath);
+            return parseUrl.toString();
+        }
+        catch (MalformedURLException e) {
+            return "";
+        }
+    }
 }
