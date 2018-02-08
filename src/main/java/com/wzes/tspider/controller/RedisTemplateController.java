c0e1e10c581f6085987169d046bec8d06714885a
wzes <793890838@qq.com>
2018-02-08 12:52:45 +0800

    添加 redis 分布式锁

---
+++ src/main/java/com/wzes/tspider/controller/RedisTemplateController.java
@@ -0,0 +1,8 @@
+package com.wzes.tspider.controller;
+
+/**
+ * @author Create by xuantang
+ * @date on 2/8/18
+ */
+public class RedisTemplateController {
+}
---
+++ src/test/java/com/wzes/tspider/RedisLockTests.java
@@ -0,0 +1,8 @@
+package com.wzes.tspider;
+
+/**
+ * @author Create by xuantang
+ * @date on 2/7/18
+ */
+public class RedisLockTests {
+}
